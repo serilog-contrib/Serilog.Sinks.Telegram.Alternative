c8f57e3ed09b98517c0694fa14d035639f8a32d9
SeppPenner <HansM2013@gmx.de>
2020-09-29 08:39:57 +0200

    Added script to push packages.

---
+++ PushPackage.bat
@@ -0,0 +1,3 @@
+dotnet nuget push "src\Serilog.Sinks.Telegram\bin\Release\HaemmerElectronics.SeppPenner.Serilog.Sinks.Telegram.*.nupkg" -s "github" --skip-duplicate
+dotnet nuget push "src\Serilog.Sinks.Telegram\bin\Release\HaemmerElectronics.SeppPenner.Serilog.Sinks.Telegram.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
+PAUSE
