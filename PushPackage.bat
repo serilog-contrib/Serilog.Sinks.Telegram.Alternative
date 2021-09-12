22c69ca9d1bd5f5349fe313059d82b02ec7ec120
SeppPenner <HansM2013@gmx.de>
2021-09-12 13:45:43 +0200

    Removed package push to my private packages (Don't exist anymore with project under the hood of serilog-contrib).

--- PushPackage.bat
+++ PushPackage.bat
@@ -1,3 +1,2 @@
-dotnet nuget push "src\Serilog.Sinks.Telegram\bin\Release\Serilog.Sinks.Telegram.Alternative.*.nupkg" -s "github" --skip-duplicate
 dotnet nuget push "src\Serilog.Sinks.Telegram\bin\Release\Serilog.Sinks.Telegram.Alternative.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
 PAUSE
